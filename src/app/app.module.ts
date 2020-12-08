9be408b6e32c99a0de909684b23d2a380c9c2431
Alexander Momot <highter1997@gmail.com>
2020-12-08 12:38:04 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
